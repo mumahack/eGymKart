1d00dbb6ae2dee4dbb2f2bc89c6c92b2c5713ca9
David Losert <david.losert@virtual-identity.com>
2017-10-07 20:31:13 +0200

    [feature] Implemented a listener for the steering of mario kart

--- src/app/egym-adapter/egym-adapter.ts
+++ src/app/egym-adapter/egym-adapter.ts
@@ -1,32 +1,38 @@
 import {translate} from "./translator";
+import {EGymMessage} from './EGymMessage';
 
+const OUR_CHIP = ''
 var url = "tcp://35.195.199.160:5556";
 
 var zmq = require('zeromq')
-    , sock = zmq.socket('sub');
+  , sock = zmq.socket('sub');
 
 sock.connect(url);
 sock.subscribe('');
 console.log('Subscriber connected');
 
+const filterCommands = [
+  'training_weight_data',
+  'training_position_data'
+];
+
+
 sock.on('message', function (e) {
-    e = e.toString("utf8");
-    let message = splitData(e);
-
-    if (!isPlayer(message.object.rfid)) return console.log(message);
-
-    translate(message);
+  e = e.toString("utf8");
+  const message: EGymMessage = splitData(e);
+  if (isPlayer(message.object.rfid)) translate(message);
 });
 
 function splitData(data) {
-    return {
-        'command': data.substr(0, data.indexOf(" ")),
-        'object': JSON.parse(data.substr(data.indexOf(" ")))
-    };
+  return {
+	'command': data.substr(0, data.indexOf(" ")),
+	'object': JSON.parse(data.substr(data.indexOf(" ")))
+  };
 }
 
-let playerIDs = [];
+let playerIDs = ['14d6ee20', '2e503f45'];
 
 function isPlayer(rfid) {
-    return playerIDs.find((id) => rfid === id) >= 0;
+  return playerIDs.find((id) => rfid === id);
 }
+
