98677e318114d664a214b2950ef082a38f773e06
David Losert <david.losert@virtual-identity.com>
2017-10-07 16:27:26 +0200

    [feature] Added robotJS and implemented a POC to control mario kart

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
+import {keyTap, keyToggle} from 'robotjs';
+
+keyToggle('e', 'down');
+keyToggle('left', 'down');
+const interval = setInterval(() => {
+  // keyTap('e');
+}, 1);
+// const interval2 = setInterval(() => {
+//   keyTap('left');
+// }, 1);
+setTimeout(() => {
+  clearInterval(interval);
+  keyToggle('e', 'up');
+  keyToggle('left', 'up');
+  // clearInterval(interval2);
+  // keyTap('shift');
+  // keep the server alive
+}, 3000);
 console.log('Running!');
