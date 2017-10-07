1717d523ff04f698133863c3e6d81e445cd7bc47
Gott50 <xxGott50xx@gmail.com>
2017-10-07 17:04:02 +0200

    clean up

--- src/app/egym-adapter/egym-adapter.ts
+++ src/app/egym-adapter/egym-adapter.ts
@@ -5,27 +5,18 @@ var zmq = require('zeromq')
 
 sock.connect(url);
 sock.subscribe('');
-console.log('Subscriber connected to port 3000');
+console.log('Subscriber connected');
 
-var position = 0;
 sock.on('message', function(e) {
-
-    // console.log("Received:", JSON.parse(e));
-
     e = e.toString("utf8");
-    console.log("utf8:", (e));
+    e = splitData(e);
+    console.log("Data Received: " + e);
 
-    let message = splitData(e);
-
-    console.log(message)
-    //
-    // console.log("Data Received: " + e.data);
-    // var message = splitData(e.data);
-    // switch (message.command) {
-    //     case "training_position_data":
-    //         position = message.object.payload.position;
-    //         break;
-    // }
+    var message = splitData(e.data);
+    switch (message.command) {
+        case "training_position_data":
+            break;
+    }
 });
 
 function splitData(data) {
