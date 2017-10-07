d1c37bfdacc0deb9b1ec91830105af92e20e9ceb
Gott50 <xxGott50xx@gmail.com>
2017-10-07 16:56:38 +0200

    get Data form eGym Machine Stream

--- src/app/egym-adapter/egym-adapter.ts
+++ src/app/egym-adapter/egym-adapter.ts
@@ -7,6 +7,30 @@ sock.connect(url);
 sock.subscribe('');
 console.log('Subscriber connected to port 3000');
 
-sock.on('message', function(topic, message) {
-    console.log('received a message related to:', topic, 'containing message:', message);
+var position = 0;
+sock.on('message', function(e) {
+
+    // console.log("Received:", JSON.parse(e));
+
+    e = e.toString("utf8");
+    console.log("utf8:", (e));
+
+    let message = splitData(e);
+
+    console.log(message)
+    //
+    // console.log("Data Received: " + e.data);
+    // var message = splitData(e.data);
+    // switch (message.command) {
+    //     case "training_position_data":
+    //         position = message.object.payload.position;
+    //         break;
+    // }
 });
+
+function splitData(data) {
+    return {
+        'command': data.substr(0, data.indexOf(" ")),
+        'object': JSON.parse(data.substr(data.indexOf(" ")))
+    };
+}
