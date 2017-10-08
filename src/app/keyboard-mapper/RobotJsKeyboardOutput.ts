72509ccd45d1a5bbeeb97a68ef82eab97f97577d
David Losert <david.losert@virtual-identity.com>
2017-10-08 09:28:55 +0200

    [chore] Deleted unused files and moved GameController to game-controller folder

--- src/app/keyboard-mapper/RobotJsKeyboardOutput.ts
+++
@@ -1,7 +0,0 @@
-import {KeyboardOutput} from './KeyboardOutput';
-import {keyToggle} from 'robotjs';
-
-export const RobotJSKeyboardOutput: KeyboardOutput = {
-  keyPress: (key: string) => {keyToggle(key, 'down')},
-  keyRelease: (key: string) => {keyToggle(key, 'up')}
-};
